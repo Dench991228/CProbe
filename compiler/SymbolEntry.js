86aa1b875ef0cf05eafc45a80c04b012623698c8
Dench991228 <denghanzhi85139@163.com>
2021-02-06 22:58:22 +0800

    skeleton of symbol entry

--- compiler/SymbolEntry.js
+++ compiler/SymbolEntry.js
@@ -1,3 +1,22 @@
 function SymbolEntry(){
+    /*basic part of the symbol*/
+    this.IdentifierType = undefined;// variable/function/struct/enumeration/typedef
+    this.IsConstant = [];// is the pointer a constant.
+    this.DataType = undefined;// the final type of the identifier.
+
+    /*things about array*/
+    this.Sizes = [];// the size of dimensions of array.
+    this.Dimensions = 0;// number of dimensions of the array.
+
+    /*things about enumeration*/
+    this.Constants = {};
+
+    /*things about struct*/
+    this.StructTable = new SymbolTable();
+
+    /*things about function*/
+    this.ReturnType = undefined;
+    this.ParameterList = [];
+
     return this
 }
